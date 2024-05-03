05b295c6b596a48daba5aeffa38ba5def37706f3
ManiTeja <44355345+manite97@users.noreply.github.com>
2024-05-03 20:11:36 +0530

    Update method to handle custom delimiters add.rb

--- add.rb
+++ add.rb
@@ -1,7 +1,14 @@
 def add(numbers)
 return 0 if numbers.empty?
 delimiter = ",|\\n"
+
+if numbers.start_with?("//")
+  delimiter = numbers[2]
+  numbers = numbers.split("\n")[1]
+end
+  
 numbers = numbers.split(/#{delimiter}/)
+
 
 
   sum = 0
