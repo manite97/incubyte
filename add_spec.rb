e94a2becf9d96f150299e6ebd169e964c7974c57
ManiTeja <44355345+manite97@users.noreply.github.com>
2024-05-03 19:38:48 +0530

    add few more test cases of single number string

--- add_spec.rb
+++ add_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe 'add' do
       expect(add('')).to eq(0)
     end
   end
+  context 'with a single number' do
+    it 'returns the number itself' do
+      expect(add('1')).to eq(1)
+    end
+
+    it 'returns the number itself' do
+      expect(add('5')).to eq(5)
+    end
+  end
 end
